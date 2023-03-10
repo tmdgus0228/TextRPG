4bb2bba852446298ffefb827211289090e4118c0
tmdgus0228 <83226327+tmdgus0228@users.noreply.github.com>
2023-03-10 14:01:02 +0900

    TextRPG 만들기_2021 토이프로젝트

---
+++ TextRPG/TextRPG/Program.cs
@@ -0,0 +1,207 @@
+﻿
+using System;
+
+namespace TextRPG
+{
+    class Program
+    {
+        enum ClassType
+        {
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+        }
+
+        struct Player
+        {
+            public int hp;
+            public int attack;
+        }
+        
+        enum MonsterType
+        {
+            None = 0,
+            Slime = 1,
+            Orc = 2,
+            Skeleton = 3
+        }
+        struct Monster
+        {
+            public int hp;
+            public int attack;
+        }
+
+        static ClassType ChooseClass()
+        {
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사");
+            Console.WriteLine("[2] 궁수");
+            Console.WriteLine("[3] 법사");
+
+            ClassType choice = ClassType.None;
+            string input = Console.ReadLine();  // 입력 받기
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.Knight;
+                    break;
+                case "2":
+                    choice = ClassType.Archer;
+                    break;
+                case "3":
+                    choice = ClassType.Mage;
+                    break;
+            }
+
+            return choice;
+        }
+
+        static void CreatePlayer(ClassType choice, out Player player)
+        {
+            // 기사(100/10) 궁수(75/12) 법사(50/15)
+            switch (choice)
+            {
+                case ClassType.Knight:
+                    player.hp = 100;
+                    player.attack = 10;
+                    break;
+                case ClassType.Archer:
+                    player.hp = 75;
+                    player.attack = 12;
+                    break;
+                case ClassType.Mage:
+                    player.hp = 50;
+                    player.attack = 15;
+                    break;
+                default:
+                    player.hp = 0;
+                    player.attack = 0;
+                    break;
+            }
+        }
+
+        static void CreateRandomMonster(out Monster monsrter)
+        {
+            Random rand = new Random();
+            int randMonster = rand.Next(1, 4);
+            switch (randMonster)
+            {
+                case (int)MonsterType.Slime:
+                    Console.WriteLine("슬라임이 스폰되었습니다!");
+                    monsrter.hp = 20;
+                    monsrter.attack = 2;
+                    break;
+                case (int)MonsterType.Orc:
+                    Console.WriteLine("오크가 스폰되었습니다!");
+                    monsrter.hp = 40;
+                    monsrter.attack = 4;
+                    break;
+                case (int)MonsterType.Skeleton:
+                    Console.WriteLine("스켈레톤이 스폰되었습니다!");
+                    monsrter.hp = 30;
+                    monsrter.attack = 3;
+                    break;
+                default:
+                    monsrter.hp = 0;
+                    monsrter.attack = 0;
+                    break;
+            }
+            
+        }
+
+        static void Fight(ref Player player, ref Monster monster)
+        {
+            while (true)
+            {
+                // 플레이어가 몬스터 공격
+                monster.hp -= player.attack;
+                if (monster.hp <= 0)
+                {
+                    Console.WriteLine("승리했습니다!");
+                    Console.WriteLine($"남은 체력: {player.hp}");
+                    break;
+                }
+
+                // 몬스터 반격
+                player.hp -= monster.attack;
+                if (player.hp <= 0)
+                {
+                    Console.WriteLine("패배했습니다!");
+                    break;
+                }
+            }
+        }
+
+        static void EnterField(ref Player player)
+        {
+            while (true)
+            {
+                Console.WriteLine("필드에 접속했습니다!");
+
+                // 랜덤으로 1~3 몬스터 중 하나를 리스폰
+                Monster monster;
+                CreateRandomMonster(out monster);
+
+                Console.WriteLine("[1] 전투 모드로 돌입");
+                Console.WriteLine("[2] 일정 확률로 마을로 도망");
+
+                string input = Console.ReadLine();
+                if (input == "1")
+                {
+                    Fight(ref player, ref monster);
+                }
+                else if (input == "2")
+                {
+                    Random rand = new Random();
+                    int randValue = rand.Next(0, 100);
+                    if (randValue <= 33)
+                    {
+                        Console.WriteLine("도망치는데 성공했습니다!");
+                        break;
+                    }
+                    else
+                    {
+                        Fight(ref player, ref monster);
+                    }
+                }
+            }
+           
+        }
+        static void EnterGame(ref Player player)
+        {
+            Console.WriteLine("마을에 접속했습니다!");
+            Console.WriteLine("[1] 월드로 간다");
+            Console.WriteLine("[2] 로비로 돌아가기");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    EnterField(ref player);
+                    break;
+                case "2":
+                    return;  // EnterGame함수에서 빠져나옴
+                    
+            }
+        }
+        static void Main(string[] args)
+        {            
+            while (true)
+            {
+                ClassType choice = ChooseClass();
+                if (choice == ClassType.None)
+                    continue;
+                
+                // 캐릭터 생성
+                Player player;                  
+                CreatePlayer(choice, out player);
+
+                // 필드로 가서 몬스터랑 싸운다
+                EnterGame(ref player);
+                
+                    
+            }
+        }
+    }
+}
